2f314b9eb94b119f275321a71d62dc9cfbf7cf8c
alEX860111 <alex86011119@gmail.com>
2014-11-23 14:28:43 +0100

    added start stop button

--- public/js/app.js
+++ public/js/app.js
@@ -6,8 +6,31 @@ app.controller("myctrl", function($scope, $interval, boardService) {
   $scope.board[2][2] = true;
   $scope.board[3][2] = true;
 
-  $interval(function() {
-    $scope.board = boardService.getNext($scope.board);
-  }, 2000);
+  var promise;
+  $scope.isActive = false;
+
+  $scope.$watch('isActive', function() {
+    $scope.buttonValue = $scope.isActive ? "Stop" : "Start";
+  });
+
+  $scope.toggleActive = function() {
+    if ($scope.isActive) {
+      stop();
+    } else {
+      start();
+    }
+  };
+
+  function start() {
+    promise = $interval(function() {
+      $scope.board = boardService.getNext($scope.board);
+    }, 2000);
+    $scope.isActive = true;
+  }
+
+  function stop() {
+    $interval.cancel(promise);
+    $scope.isActive = false;
+  }
 
 });
