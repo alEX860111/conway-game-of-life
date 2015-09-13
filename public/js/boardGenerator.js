3c93b0018cf7d08b9b8df2ebd5c733ba3a6db4c6
alEX860111 <alex86011119@gmail.com>
2015-09-13 13:05:22 +0200

    refactored

--- public/js/boardGenerator.js
+++ public/js/boardGenerator.js
@@ -1,52 +1,48 @@
 angular.module("services")
 
 .factory("boardGenerator", ["matrixGenerator", "cellGenerator", function(matrixGenerator, cellGenerator) {
+	function generateMatrix(size) {
+		return matrixGenerator.generateMatrix(size, cellGenerator.generateCell);
+	}
+
+	function createBoard(matrix) {
+		return {
+			getCell: function(rowIdx, colIdx) {
+				return matrix.getElement(rowIdx, colIdx);
+			},
+			reset: function() {
+				matrix.visitAllElements(function(cell) {
+					cell.isAlive = false;
+				});
+			},
+			areAllCellsDead: function() {
+				var result = true;
+				matrix.visitAllElements(function(cell) {
+					if (cell.isAlive) {
+						result = false;
+					}
+				});
+				return result;
+			},
+			getNext: function() {
+				var nextMatrix = generateMatrix(matrix.getSize());
+				matrix.visitAllElements(function(cell, rowIdx, colIdx) {
+					var numAliveNeighbors = 0;
+					matrix.visitNeighborElements(rowIdx, colIdx, function(cell) {
+						if (cell.isAlive) {
+							numAliveNeighbors++;
+						}
+					});
+					nextMatrix.getElement(rowIdx, colIdx).isAlive = cell.survives(numAliveNeighbors);
+				});
+				return createBoard(nextMatrix);
+			}
+		};
+	}
+
 	return {
 		generateBoard: function(size) {
-
-			function generateMatrix() {
-				return matrixGenerator.generateMatrix(size, cellGenerator.generateCell);
-			}
-
-			var matrix = generateMatrix();
-
-			function createBoard(matrix) {
-
-				return {
-					getCell: function(rowIdx, colIdx) {
-						return matrix.getElement(rowIdx, colIdx);
-					},
-					reset: function() {
-						matrix.visitAllElements(function(cell) {
-							cell.isAlive = false;
-						});
-					},
-					areAllCellsDead: function() {
-						var result = true;
-						matrix.visitAllElements(function(cell) {
-							if (cell.isAlive) {
-								result = false;
-							}
-						});
-						return result;
-					},
-					getNext: function() {
-						var nextMatrix = generateMatrix();
-						matrix.visitAllElements(function(cell, rowIdx, colIdx) {
-							var numAliveNeighbors = 0;
-							matrix.visitNeighborElements(rowIdx, colIdx, function(cell) {
-								if (cell.isAlive) {
-									numAliveNeighbors++;
-								}
-							});
-							nextMatrix.getElement(rowIdx, colIdx).isAlive = cell.survives(numAliveNeighbors);
-						});
-						return createBoard(nextMatrix);
-					}
-				};
-
-			};
-
+			var matrix = generateMatrix(size);
 			return createBoard(matrix);
 		}
 	};
