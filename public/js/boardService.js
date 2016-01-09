1c1ff07304c5ec7e6118209f1fd6eaee27dc25ad
Alexander Buhr <alex86011119@googlemail.com>
2016-01-09 11:44:58 +0100

    More patterns ^

--- public/js/boardService.js
+++ public/js/boardService.js
@@ -13,17 +13,6 @@ angular.module("services")
 					return board;
 				}
 			}, {
-				title: "Glider",
-				createBoard: function() {
-					var board = boardGenerator.generateBoard(10);
-					board.getCell(0, 0).isAlive = true;
-					board.getCell(1, 1).isAlive = true;
-					board.getCell(1, 2).isAlive = true;
-					board.getCell(2, 0).isAlive = true;
-					board.getCell(2, 1).isAlive = true;
-					return board;
-				}
-			}, {
 				title: "Pulsar",
 				createBoard: function() {
 					var board = boardGenerator.generateBoard(17);
@@ -87,54 +76,91 @@ angular.module("services")
 					return board;
 				}
 			}, {
-				title: "Gosper glider gun",
+				title: "Glider",
 				createBoard: function() {
-					var board = boardGenerator.generateBoard(38);
-					board.getCell(1, 25).isAlive = true;
+					var board = boardGenerator.generateBoard(10);
+					board.getCell(0, 0).isAlive = true;
+					board.getCell(1, 1).isAlive = true;
+					board.getCell(1, 2).isAlive = true;
+					board.getCell(2, 0).isAlive = true;
+					board.getCell(2, 1).isAlive = true;
+					return board;
+				}
+			}, {
+				title: "Lightweight spaceship (LWSS)",
+				createBoard: function() {
+					var board = boardGenerator.generateBoard(10);
+					board.getCell(1, 3).isAlive = true;
+					board.getCell(1, 6).isAlive = true;
 
-					board.getCell(2, 23).isAlive = true;
-					board.getCell(2, 25).isAlive = true;
+					board.getCell(2, 2).isAlive = true;
 
-					board.getCell(3, 13).isAlive = true;
-					board.getCell(3, 14).isAlive = true;
-					board.getCell(3, 21).isAlive = true;
-					board.getCell(3, 22).isAlive = true;
-					board.getCell(3, 35).isAlive = true;
-					board.getCell(3, 36).isAlive = true;
+					board.getCell(3, 2).isAlive = true;
+					board.getCell(3, 6).isAlive = true;
 
-					board.getCell(4, 12).isAlive = true;
+					board.getCell(4, 2).isAlive = true;
+					board.getCell(4, 3).isAlive = true;
+					board.getCell(4, 4).isAlive = true;
+					board.getCell(4, 5).isAlive = true;
+					return board;
+				}
+			}, {
+				title: "David Eppstein's weekender",
+				createBoard: function() {
+					var board = boardGenerator.generateBoard(20);
+					board.getCell(2, 2).isAlive = true;
+					board.getCell(2, 3).isAlive = true;
+					board.getCell(2, 4).isAlive = true;
+					board.getCell(2, 15).isAlive = true;
+					board.getCell(2, 16).isAlive = true;
+					board.getCell(2, 17).isAlive = true;
+
+					board.getCell(3, 2).isAlive = true;
+					board.getCell(3, 4).isAlive = true;
+					board.getCell(3, 15).isAlive = true;
+					board.getCell(3, 17).isAlive = true;
+
+					board.getCell(4, 2).isAlive = true;
+					board.getCell(4, 3).isAlive = true;
+					board.getCell(4, 4).isAlive = true;
+					board.getCell(4, 15).isAlive = true;
 					board.getCell(4, 16).isAlive = true;
-					board.getCell(4, 21).isAlive = true;
-					board.getCell(4, 22).isAlive = true;
-					board.getCell(4, 35).isAlive = true;
-					board.getCell(4, 36).isAlive = true;
+					board.getCell(4, 17).isAlive = true;
 
-					board.getCell(5, 1).isAlive = true;
-					board.getCell(5, 2).isAlive = true;
-					board.getCell(5, 11).isAlive = true;
-					board.getCell(5, 17).isAlive = true;
-					board.getCell(5, 21).isAlive = true;
-					board.getCell(5, 22).isAlive = true;
+					board.getCell(5, 3).isAlive = true;
+					board.getCell(5, 4).isAlive = true;
+					board.getCell(5, 9).isAlive = true;
+					board.getCell(5, 10).isAlive = true;
+					board.getCell(5, 15).isAlive = true;
+					board.getCell(5, 16).isAlive = true;
 
-					board.getCell(6, 1).isAlive = true;
-					board.getCell(6, 2).isAlive = true;
+					board.getCell(6, 8).isAlive = true;
 					board.getCell(6, 11).isAlive = true;
-					board.getCell(6, 15).isAlive = true;
-					board.getCell(6, 17).isAlive = true;
-					board.getCell(6, 18).isAlive = true;
-					board.getCell(6, 23).isAlive = true;
-					board.getCell(6, 25).isAlive = true;
 
-					board.getCell(7, 11).isAlive = true;
-					board.getCell(7, 17).isAlive = true;
-					board.getCell(7, 25).isAlive = true;
+					board.getCell(7, 4).isAlive = true;
+					board.getCell(7, 6).isAlive = true;
+					board.getCell(7, 13).isAlive = true;
+					board.getCell(7, 15).isAlive = true;
 
+					board.getCell(8, 5).isAlive = true;
+					board.getCell(8, 6).isAlive = true;
+					board.getCell(8, 7).isAlive = true;
+					board.getCell(8, 8).isAlive = true;
+					board.getCell(8, 9).isAlive = true;
+					board.getCell(8, 10).isAlive = true;
+					board.getCell(8, 11).isAlive = true;
 					board.getCell(8, 12).isAlive = true;
-					board.getCell(8, 16).isAlive = true;
+					board.getCell(8, 13).isAlive = true;
+					board.getCell(8, 14).isAlive = true;
 
-					board.getCell(9, 13).isAlive = true;
-					board.getCell(9, 14).isAlive = true;
+					board.getCell(9, 7).isAlive = true;
+					board.getCell(9, 12).isAlive = true;
 
+					board.getCell(10, 7).isAlive = true;
+					board.getCell(10, 12).isAlive = true;
+
+					board.getCell(11, 7).isAlive = true;
+					board.getCell(11, 12).isAlive = true;
 					return board;
 				}
 			}, {
